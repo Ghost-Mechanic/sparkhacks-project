8f300af032f0ae4726677431fa03cf8238211089
cl-py <ivypool59@gmail.com>
2025-02-08 02:45:36 -0600

    added a "like" table to sql. swipe_logic now handles swiping left and right and "liking" businesses.

--- backend/project/schema.sql
+++ backend/project/schema.sql
@@ -7,6 +7,13 @@ CREATE TABLE users (
   password TEXT NOT NULL
 );
 
+CREATE TABLE likedBusinesses (
+  username TEXT NOT NULL,
+  business_name TEXT NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username),
+  FOREIGN KEY (business_name) REFERENCES business(business_name)
+);
+
 CREATE TABLE businesses (
   business_id INTEGER PRIMARY KEY AUTOINCREMENT,
   business_name TEXT UNIQUE NOT NULL,
--- backend/project/swipe/swipe_logic.py
+++ backend/project/swipe/swipe_logic.py
@@ -1,7 +1,37 @@
 # ABOUT: This page handles logic relating to displaying the business profiles on the
 # swipe page.
 
-from flask import Blueprint, request, redirect, url_for, session
+from flask import Blueprint, request, redirect, url_for, session, jsonify
 from project.db import get_db
 
 swipe = Blueprint('swipe', __name__)
+
+@swipe.route('/swipe/<string:business_name>', methods=['GET'])
+def business_profile(business_name):
+    db = get_db()
+
+    # We ping the database to retrieve the current business' info!
+    business = db.execute("SELECT business_name, business_type, about_me FROM businesses WHERE business_name = ?", (business_name,)).fetchone()
+
+    # we're handling a right "swipe", aka "liking".
+    if request.method == 'POST' and 'like' in request.form:
+        username = session.get('user_id') # we get the current user's username
+
+        #now let's insert the business into the current user's "likes"
+        try:
+            db.execute("INSERT INTO likedBusinesses (username, business_name) VALUES (?, ?)", 
+                       (username, business_name))
+            db.commit()
+    
+    # now we get the info of the next business
+    next_business = db.execute("SELECT business_name FROM businesses where business_name != ? ORDER BY RANDOM() LIMIT 1",
+                                (business_name),).fetchone()
+
+    # returning information about the current business and next business to be displayed to the front-end.
+    return jsonify({
+        'business_name': business['business_name'],
+        'business_type': business['business_type'],
+        'about_me': business['about_me']
+    },
+    'next_business': next_business['business_name']) # /swipe/<next_business_name> to create our business loop by navigating to the next business.
+
