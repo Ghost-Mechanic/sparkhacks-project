b428a3ee53b830dc0a7b9bcb666e5f9852010134
Ghost-Mechanic <jcerv6@uic.edu>
2025-02-07 23:27:55 -0600

    Created skeleton for main swiping page

---
+++ spark/src/SwipePage.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import "./styles/SwipePage.css";
+import forumImage from "./assets/forumsIcon.png";
+import swipeImage from "./assets/mainSwipeIcon.png";
+import profileImage from "./assets/userProfileIcon.png";
+
+function SwipePage() {
+
+    return (
+        <>
+            <div className="main-container">
+                <div className="app-name">
+                    <h1>App Name</h1>
+                </div>
+                <div className="business-card">
+
+                </div>
+                <footer className="footer">
+                    <div className="footer-images" >
+                        <img src={forumImage} height="32px" width="32px" />
+                        <img src={swipeImage} height="44px" width="44px" />
+                        <img src={profileImage} height="32px" width="32px" />
+                    </div>
+                </footer>
+            </div>
+        </>
+    )
+
+}
+
+export default SwipePage;
